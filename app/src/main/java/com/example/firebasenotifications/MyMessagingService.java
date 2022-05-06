d2316bc69b2a67ab3c8003b35f6a2a6050f53057
muhammadumai699 <95739595+muhammadumai699@users.noreply.github.com>
2022-05-06 14:46:15 +0500

    Firebase notifications

--- app/src/main/java/com/example/firebasenotifications/MyMessagingService.java
+++ app/src/main/java/com/example/firebasenotifications/MyMessagingService.java
@@ -1,5 +1,8 @@
 package com.example.firebasenotifications;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -17,12 +20,22 @@ public class MyMessagingService  extends FirebaseMessagingService {
 
 
     public void showNotification(String title, String message){
+
+        Intent intent = new Intent(MyMessagingService.this,MainActivity2.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MyMessagingService.this,
+                1,intent,PendingIntent.FLAG_ONE_SHOT);
+
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
                 "MyNotifications")
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setAutoCancel(true)
-                .setContentText(message);
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
         managerCompat.notify(999,builder.build());
